e0949126d0c8d7cbc0ff1a19d757c789a2ba2650
hernancaceres <harinatha777@hotmail.com>
2022-10-13 20:13:14 -0300

    Funcionando la conexion a la base de datos

---
+++ src/test/java/com/proyectoap2/ap2/Ap2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoap2.ap2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ap2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
