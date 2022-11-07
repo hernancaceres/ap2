fb3593549b65493b981843790cf24df1671e82c6
hernancaceres <harinatha777@hotmail.com>
2022-11-07 17:28:10 -0300

    resolviendocors en la nube

--- src/main/java/com/proyectoap2/ap2/Controller/ExperienciaController.java
+++ src/main/java/com/proyectoap2/ap2/Controller/ExperienciaController.java
@@ -22,8 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/experiencia")
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = "https://hcfrontend.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendap2.web.app")
 public class ExperienciaController {
 
     @Autowired
