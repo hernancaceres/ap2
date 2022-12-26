be0120ad31060300c2a2a636e07b033121774fed
hernancaceres <harinatha777@hotmail.com>
2022-12-26 17:57:06 -0300

    CORS PARA "https://ap2-production.up.railway.app/" FUNCIONANDO333333

--- src/main/java/com/proyectoap2/ap2/Entity/Persona.java
+++ src/main/java/com/proyectoap2/ap2/Entity/Persona.java
@@ -29,8 +29,7 @@ public class Persona {
     private String apellido;
 
     @NotNull
-    @Column(length = 1000)
-    @Size(min = 3, max = 1000)
+   
     private String descripcion;
 
     private String img;
